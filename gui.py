6589a5d0b103d6e9efb79f5b4e1bd87c7d2f7fa4
Nethravathi15 <nethravathi.k6212@gmail.com>
2024-06-19 17:04:10 +0530

    Created Basic GUI without interactions

--- gui.py
+++ gui.py
@@ -5,6 +5,8 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
 window.read()
 window.close()
